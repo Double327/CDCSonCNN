990a825cd01e18fe0f82ed6d4768eda1f3b88437
Double327 <180303665@qq.com>
2020-08-19 17:17:16 +0800

    初始框架

---
+++ src/test/java/cn/doublefloat/dachuang/DachuangApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.doublefloat.dachuang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DachuangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
