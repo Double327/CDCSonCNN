c0d3730b07385436aa826a9e8856e670e15527b9
Double327 <180303665@qq.com>
2020-10-15 10:01:15 +0800

    Swagger添加Header参数

--- src/main/java/cn/doublefloat/dachuang/framework/config/Swagger2Config.java
+++ src/main/java/cn/doublefloat/dachuang/framework/config/Swagger2Config.java
@@ -3,11 +3,17 @@ package cn.doublefloat.dachuang.framework.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 李广帅
@@ -19,7 +25,13 @@ public class Swagger2Config {
 
     @Bean
     public Docket webApiConfig() {
+        ParameterBuilder tokenParam = new ParameterBuilder();
+        tokenParam.name("token").description("请求Token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(tokenParam.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(parameters)
                 .apiInfo(webApiInfo());
     }
 
