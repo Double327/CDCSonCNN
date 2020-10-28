e4ee2aab246aa83b1427829c04d6defbe1e98e36
BlackCrafts <13160292800@163.com>
2020-10-28 18:53:50 +0800

    部分完善;

--- src/test/java/cn/doublefloat/CDCSonCNN/project/student/utils/CodesCompareTest.java
+++ src/test/java/cn/doublefloat/CDCSonCNN/project/student/utils/CodesCompareTest.java
@@ -2,18 +2,17 @@ package cn.doublefloat.CDCSonCNN.project.student.utils;
 
 import cn.doublefloat.CDCSonCNN.common.utils.code_anlysis.algorithm.duplicateRemovalAnlysis.CodesCompare;
 import org.junit.jupiter.api.Test;
-
 import java.math.BigDecimal;
 
 /**
  * @author 魏荣轩
  * @date 2020/10/28 18:12
  */
-public class CodesCompareTest {
+class CodesCompareTest {
     @Test
-    public void textCompare(){
-        System.out.println("TestCode1.cpp & TestCode2.cpp = "+
+    void textCompare(){
+        System.out.println("codes/Txt/TestCode1.txt 查重 codes/Cpp/TestCode2.cpp 相似度: "+
                 new BigDecimal(CodesCompare.getCodesSimilarity("codes/Txt/TestCode1.txt", "codes/Cpp/TestCode2.cpp"))
-                        .setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue()+"%");
+                        .setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue()+" %");
     }
 }
