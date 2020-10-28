59f8cadc51c81348bd622c0bf1077272441cf13a
BlackCrafts <13160292800@163.com>
2020-10-28 18:25:25 +0800

    完善C++基础查重模块； 文件读取兼容至项目；

--- src/main/java/cn/doublefloat/CDCSonCNN/common/utils/code_anlysis/algorithm/duplicateRemovalAnlysis/CodesCompare.java
+++ src/main/java/cn/doublefloat/CDCSonCNN/common/utils/code_anlysis/algorithm/duplicateRemovalAnlysis/CodesCompare.java
@@ -3,11 +3,9 @@ package cn.doublefloat.CDCSonCNN.common.utils.code_anlysis.algorithm.duplicateRe
 import cn.doublefloat.CDCSonCNN.common.utils.code_anlysis.algorithm.duplicateRemovalAnlysis.abstractCompare.AbstractCompare;
 import cn.doublefloat.CDCSonCNN.common.utils.code_anlysis.algorithm.duplicateRemovalAnlysis.algorithmUtils.DelComments;
 import cn.doublefloat.CDCSonCNN.common.utils.code_anlysis.algorithm.duplicateRemovalAnlysis.algorithmUtils.LD;
-import java.io.*;
-import java.util.ArrayList;
+import cn.doublefloat.CDCSonCNN.common.utils.code_anlysis.utils.TxtUtils;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Objects;
 
 /**
  * @author 魏荣轩
@@ -68,12 +66,7 @@ public class CodesCompare extends AbstractCompare {
     public String getPreProcessedCode(String filePath) {
         String code="";
         try{
-            BufferedReader br = new BufferedReader (new InputStreamReader(new FileInputStream(filePath)));
-            StringBuilder buf = new StringBuilder();
-            String line;
-            while((line=br.readLine())!=null){
-                buf.append(line).append("\n");
-            }
+            StringBuffer buf = TxtUtils.readTxtFile(filePath);
             //删除所有注释
             code = DelComments.delComments(buf.toString());
             int pos1 = 0,pos2 = 0;
@@ -108,7 +101,6 @@ public class CodesCompare extends AbstractCompare {
             code = ret.toString();
             //删除所有空格和换行
             code=code.replaceAll("\\s", "");
-            br.close();
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -128,39 +120,14 @@ public class CodesCompare extends AbstractCompare {
     }
 
     /**
-     * 测试用，暂未兼容项目
-     * 理论兼容 cpp/txt 等纯文本格式
+     * 获取两个代码文本相似度
      *
-     * @param args 主参
-     * @throws IOException 读取错误
+     * @param codeFile1Path 代码文本1地址
+     * @param codeFile2Path 代码文本2地址
+     * @return 相似度百分
      */
-    public static void main(String[]args) throws IOException {
+    public static double getCodesSimilarity(String codeFile1Path, String codeFile2Path){
         CodesCompare cmp = new CodesCompare();
-        File dic= new File("F:\\代码");
-        String[] names ={"3568.cpp","3569.cpp","3570.cpp","3571.cpp","3572.cpp"};
-        for(String name:names){
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("F:\\代码\\分析结果\\"+name)));
-            System.out.println("当前文件："+name);
-            bw.newLine();
-            ArrayList<String> idList =new ArrayList<String>();
-            ArrayList<String> codeList =new ArrayList<String>();
-            for(File f1: Objects.requireNonNull(dic.listFiles())){
-                File f2 = new File(f1.getAbsoluteFile()+"");
-                if(f2.exists()){
-                    idList.add(f1.getName());
-                    codeList.add(cmp.getPreProcessedCode(f2.getAbsolutePath()));
-                }
-            }
-            for(int i=0;i<codeList.size();i++){
-                for(int j=i+1;j<codeList.size();j++){
-                    double s = cmp.getSimilarity(codeList.get(i), codeList.get(j));
-                    if(s>=0.7){
-                        bw.write(idList.get(i)+"\t"+idList.get(j)+"\t"+s);
-                        bw.newLine();
-                    }
-                }
-            }
-            bw.close();
-        }
+        return cmp.getSimilarity(cmp.getPreProcessedCode(codeFile1Path),cmp.getPreProcessedCode(codeFile2Path));
     }
 }
---
+++ src/main/resources/codes/Cpp/TestCode2.cpp
@@ -0,0 +1,36 @@
+public class Pro39Mid {
+
+    private static List<List<Integer>> res = new ArrayList<>();
+
+    public static void main(String[] args){
+        int[] a = new int[]{2,3,4,6};
+        long start = System.currentTimeMillis();
+        System.out.println(combinationSum(a,6));
+        long end = System.currentTimeMillis();
+        System.out.println(end - start + "ms");
+    }
+
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> path = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(path,candidates,target,0,0);
+        return res;
+    }
+
+    private static void backtrack(List<Integer> path,int[] candidates,int target,int sum,int begin) {
+        if(sum == target) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+            if(rs <= target) {
+                path.add(candidates[i]);
+                backtrack(path,candidates,target,rs,i);
+                //回溯 减少 分叉数量，直至原点
+                path.remove(path.size()-1);
+            } else {
+                break;
+            }
+        }
+    }
+}
