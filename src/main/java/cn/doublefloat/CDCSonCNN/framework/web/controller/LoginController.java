2232bbcf1e8b57862c929605ce0eb2c5fdd64035
Double327 <180303665@qq.com>
2020-11-12 23:44:09 +0800

    登录模块

--- src/main/java/cn/doublefloat/CDCSonCNN/framework/web/controller/LoginController.java
+++ src/main/java/cn/doublefloat/CDCSonCNN/framework/web/controller/LoginController.java
@@ -1,12 +1,20 @@
 package cn.doublefloat.CDCSonCNN.framework.web.controller;
 
+import cn.doublefloat.CDCSonCNN.common.utils.ServletUtils;
+import cn.doublefloat.CDCSonCNN.framework.security.LoginUser;
 import cn.doublefloat.CDCSonCNN.framework.security.service.LoginService;
+import cn.doublefloat.CDCSonCNN.framework.security.service.PermissionService;
+import cn.doublefloat.CDCSonCNN.framework.security.service.TokenService;
 import cn.doublefloat.CDCSonCNN.framework.web.domain.AjaxResult;
+import cn.doublefloat.CDCSonCNN.projects.system.domain.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 /**
  * @author 李广帅
@@ -19,6 +27,12 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private PermissionService permissionService;
+
     @PostMapping("/login")
     @ApiOperation("用户登录")
     public AjaxResult login(String no, String password, String code, String uuid) {
@@ -26,5 +40,25 @@ public class LoginController {
         AjaxResult success = AjaxResult.success();
         success.put("token", token);
         return success;
+    }
+
+    /**
+     * 获取用户信息
+     *
+     * @return 用户信息
+     */
+    @GetMapping("/getUserInfo")
+    public AjaxResult getInfo() {
+        LoginUser loginUser = tokenService.getLoginUser(ServletUtils.getRequest());
+        User user = loginUser.getUser();
+        // 角色集合
+        Set<String> roles = permissionService.getRolePermission(user);
+        // 权限集合
+        Set<String> permissions = permissionService.getMenuPermission(user);
+        AjaxResult ajax = AjaxResult.success();
+        ajax.put("user", user);
+        ajax.put("roles", roles);
+        ajax.put("permissions", permissions);
+        return ajax;
     }
 }
