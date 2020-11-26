d3e5fbd1aaba096a238f973c96c5af0e29252803
Double327 <180303665@qq.com>
2020-11-26 16:04:10 +0800

    课程信息 Service服务

--- src/main/java/cn/doublefloat/CDCSonCNN/projects/course/service/impl/CourseServiceImpl.java
+++ src/main/java/cn/doublefloat/CDCSonCNN/projects/course/service/impl/CourseServiceImpl.java
@@ -1,7 +1,10 @@
 package cn.doublefloat.CDCSonCNN.projects.course.service.impl;
 
 import cn.doublefloat.CDCSonCNN.projects.course.domain.Course;
+import cn.doublefloat.CDCSonCNN.projects.course.mapper.CourseMapper;
 import cn.doublefloat.CDCSonCNN.projects.course.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -9,29 +12,39 @@ import java.util.List;
  * @author 李广帅
  * @date 2020/11/26 上午11:03
  */
+@Service
 public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    private CourseMapper courseMapper;
+
+    @Override
+    public Course findCourseById(Long id) {
+        return courseMapper.findCourseById(id);
+    }
+
     @Override
     public List<Course> list(Course course) {
-        return null;
+        return courseMapper.findCourseList(course);
     }
 
     @Override
     public int add(Course course) {
-        return 0;
+        return courseMapper.insertCourse(course);
     }
 
     @Override
     public int delete(Long[] ids) {
-        return 0;
+        return courseMapper.deleteCourseByIds(ids);
     }
 
     @Override
     public int update(Course course) {
-        return 0;
+        return courseMapper.updateCourse(course);
     }
 
     @Override
     public int changeStatus(Course course) {
-        return 0;
+        return courseMapper.changeStatus(course);
     }
 }
